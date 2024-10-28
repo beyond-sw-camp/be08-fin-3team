557cd30e705bf2333cf892553a400c38a5f6d0d5
uzz99 <yj.lee991228@gmail.com>
2024-10-28 14:39:51 +0900

    feat: #136 alert창 재사용 가능하도록 컴포넌트화

---
+++ src/components/shared/AlertComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-alert v-if="show" :type="type" class="alert" variant="tonal" >
+    <h5 class="text-h5 text-capitalize">{{ type === 'success' ? 'Success' : 'Warning' }}</h5>
+    <div>{{ message }}</div>
+  </v-alert>
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      default: 'success',
+    },
+  },
+  watch: {
+    show(newVal) {
+      if (newVal) {
+        console.log("AlertComponent 활성화:", this.message);
+      } else {
+        console.log("AlertComponent 비활성화");
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+.alert {
+  position: fixed;
+  top: 16%;
+  left: 90%;
+  transform: translateX(-50%);
+  z-index: 3000;
+  width: 100%;
+  max-width: 16%;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  font-size: 1rem;
+}
+</style>
