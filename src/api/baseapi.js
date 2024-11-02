8754d9b1c123b37098beb363e09197498b2af11b
kuk329 <kuk329@cau.ac.kr>
2024-11-02 17:24:13 +0900

    chore: 배포 url 주소 변경

--- src/api/baseapi.js
+++ src/api/baseapi.js
@@ -4,7 +4,9 @@ import axios from "axios";
 // Axios 인스턴스 생성
 const baseApi = axios.create({
   //  baseURL: 'http://localhost:8080/api',
-  baseURL: 'http://15.164.92.171:8080/api',
+ // baseURL: 'http://15.164.92.171:8080/api',
+ baseURL: 'http://api.salesboost.store/api',
+
   });
 
 
