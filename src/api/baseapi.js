ac35298899f91bf282a0d0630c89dd1f9dc49a76
kuk329 <kuk329@cau.ac.kr>
2024-11-03 19:26:46 +0900

    feat: 인증 코드 전송 시 응답 대기 중 로링바 표시 기능 추가

--- src/api/baseapi.js
+++ src/api/baseapi.js
@@ -3,9 +3,9 @@ import axios from "axios";
 
 // Axios 인스턴스 생성
 const baseApi = axios.create({
-  //  baseURL: 'http://localhost:8080/api',
+    baseURL: 'http://localhost:8080/api',
  // baseURL: 'http://15.164.92.171:8080/api',
- baseURL: 'https://api.salesboost.store/api',
+// baseURL: 'https://api.salesboost.store/api',
 
   });
 
