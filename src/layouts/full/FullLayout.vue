74d7c550d50a3e2aaa63fb8b076bd9714ecb1d66
uzz99 <yj.lee991228@gmail.com>
2024-11-04 15:55:13 +0900

    fix: #196 사이드바 활성화
    - 유저 프로필 부분 변경(이름 및 로그아웃만 남김)
    - 창 축소 로고 수정
    - 아이콘 등 스타일 변경

--- src/layouts/full/FullLayout.vue
+++ src/layouts/full/FullLayout.vue
@@ -68,10 +68,10 @@ const customizer = useCustomizerStore();
             <v-navigation-drawer app temporary elevation="10" location="right" v-model="customizer.Customizer_drawer" width="320">
                 <Customizer />
             </v-navigation-drawer>
-            <!-- <VerticalHeaderVue v-if="!customizer.setHorizontalLayout" />
-            <VerticalSidebarVue v-if="!customizer.setHorizontalLayout" /> -->
-            <HorizontalHeader/>
-            <HorizontalSidebar/>
+            <VerticalHeaderVue v-if="!customizer.setHorizontalLayout" />
+            <VerticalSidebarVue v-if="!customizer.setHorizontalLayout" />
+            <HorizontalHeader v-if="customizer.setHorizontalLayout" />
+            <HorizontalSidebar v-if="customizer.setHorizontalLayout" />
             <v-main>
                 <v-container fluid class="page-wrapper pb-sm-15 pb-10">
                     <div :class="customizer.boxed ? 'maxWidth' : ''">
