2a4a172998df60245c8c6f46d62356fa151d7486
kuk329 <kuk329@cau.ac.kr>
2024-11-04 21:40:15 +0900

    feat: 이미지 업로드 버튼 구현

--- src/components/apps/user/MyIntroCard.vue
+++ src/components/apps/user/MyIntroCard.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts">
+<script setup>
+import api from '@/api/axiosinterceptor';
+import { ref } from 'vue';
+
 const props = defineProps({
     email:String,
     dept:String,
@@ -7,14 +10,59 @@ const props = defineProps({
     name:String
 
 })
+
+
+const selectedFile =ref() // 선택한 파일
+
+const onFileChange = (event) => {
+  const file = event.target.files[0];
+  if (file && file.type.includes('image')) {
+    selectedFile.value = file;
+  } else {
+    alert('이미지 파일만 선택가능합니다');
+    selectedFile.value = null;
+  }
+};
+
+const uploadImage = async () => {
+  if (!selectedFile.value) {
+    alert('이미지를 선택해주세요');
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append('file', selectedFile.value);
+
+  try {
+    const response = await api.post('/users/upload', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    console.log(response);
+    if(response.data.code==200){
+        console.log(response.data.result);
+
+        localStorage.setItem("profileUrl","");
+    }
+    
+    // uploadMessage.value = 'Image uploaded successfully!';
+    // uploadedImageUrl.value = response.data.imageUrl; // 서버에서 이미지 URL을 반환한다고 가정
+
+    // todo : localstorage 값 update
+  } catch (error) {
+    uploadMessage.value = 'Image upload failed.';
+    console.error(error);
+  }
+};
+
 </script>
 <template>
     <v-row>
         <v-col cols="12">
             <v-card variant="outlined" class="bg-surface">
                 <v-card-item>
-                    <!-- <h4 class="text-h4">Introduction</h4> -->
-                
                     <div class="d-flex gap-3 mb-5">
                         <BriefcaseIcon size="21" />
                         <span class="text-h6">부서 : {{ dept }}</span>
@@ -31,7 +79,34 @@ const props = defineProps({
                         <MapPinIcon size="21" />
                         <span class="text-h6">가입일 : {{ joinDate }}</span>
                     </div>
-                </v-card-item>
+                    <!-- <div clss="d-flex gap-3 mb-5">
+                        <v-btn @click="uploadImage" block color="secondary" variant="flat">사진 변경</v-btn>
+                        <input type="file" @change="onFileChange" accept="image/*">
+                    </div> -->
+
+                    <div>
+        <v-file-input
+          v-model="selectedFile"
+          placeholder="이미지 선택"
+          label="이미지 업로드"
+          multiple
+          variant="outlined"
+          hide-details="auto"
+          prepend-icon="mdi-paperclip"
+          @change="onFileChange"
+        >
+          <template v-slot:selection="{ fileNames }">
+            <template v-for="fileName in fileNames" :key="fileName">
+              <v-chip size="small" label color="primary" class="mr-2">
+                {{ fileName }}
+              </v-chip>
+            </template>
+          </template>
+        </v-file-input>
+        <v-btn @click="uploadImage" block color="secondary" variant="flat">사진 변경</v-btn>
+        </div>
+                    
+                    </v-card-item>
             </v-card>
         </v-col>
     </v-row>
